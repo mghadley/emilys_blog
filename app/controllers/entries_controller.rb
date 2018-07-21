f71af197460a9cdc1d6c8b7f3bfba69aa37d8a63
Matthew Hadley <matthew.g.hadley@gmail.com>
2018-07-21 16:59:31 -0700

    setup

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,30 @@
+class EntriesController < ApplicationController
+  def index
+    @entries = Entry.all
+  end
+
+  def show
+    @entry = Entry.find(params[:id])
+  end
+
+  def new
+    @entry = Entry.new
+  end
+
+  def create
+    @entry = Entry.new(entry_params)
+    if @entry.save
+      redirect_to entries_path
+    else
+    end
+  end
+
+  def edit
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:title, :body)
+  end
+end
